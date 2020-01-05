d933fbbd3ffbfeb75d5fa04a3c16853f86a8b91a
GERMAN GUSTAVO SANDOVAL <38325398@comunidadort.edu.ar>
2020-01-05 06:11:28 +0000

    BASE
    git-tfs-id: [https://dev.azure.com/38325398/]$/OnlineStore;C38

--- OnlineStore/OnlineStore/App_Start/IdentityConfig.cs
+++ OnlineStore/OnlineStore/App_Start/IdentityConfig.cs
@@ -53,24 +53,6 @@ namespace OnlineStore
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator();
 
-            // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
-            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
-            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
-
-            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
-            // You can write your own provider and plug it in here.
-            manager.RegisterTwoFactorProvider("Phone Code", new PhoneNumberTokenProvider<ApplicationUser>
-            {
-                MessageFormat = "Your security code is {0}"
-            });
-            manager.RegisterTwoFactorProvider("Email Code", new EmailTokenProvider<ApplicationUser>
-            {
-                Subject = "Security Code",
-                BodyFormat = "Your security code is {0}"
-            });
-            manager.EmailService = new EmailService();
-            manager.SmsService = new SmsService();
             var dataProtectionProvider = options.DataProtectionProvider;
             if (dataProtectionProvider != null)
             {
